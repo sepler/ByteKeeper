809817ad8e85dbc0cfa0cac026031c69507a4dfa
sepler <sepler@users.noreply.github.com>
2021-02-13 21:14:32 -0500

    Initial commit`

---
+++ src/dev/sepler/ByteKeeper/ByteKeeperApplication.java
@@ -0,0 +1,13 @@
+package dev.sepler.ByteKeeper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ByteKeeperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ByteKeeperApplication.class, args);
+	}
+
+}
---
+++ tst/dev/sepler/ByteKeeper/ByteKeeperApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.sepler.ByteKeeper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ByteKeeperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
