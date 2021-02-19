2262ef4af78c25826c2b3bf96da5efc3d66c2504
sepler <sepler@users.noreply.github.com>
2021-02-18 23:43:39 -0500

    Define PutFile API model

--- src/dev/sepler/bytekeeper/controller/ByteKeeperController.java
+++ src/dev/sepler/bytekeeper/controller/ByteKeeperController.java
@@ -5,6 +5,8 @@ import dev.sepler.bytekeeper.rest.GetFileRequest;
 import dev.sepler.bytekeeper.rest.GetFileResponse;
 import dev.sepler.bytekeeper.rest.GetFilesRequest;
 import dev.sepler.bytekeeper.rest.GetFilesResponse;
+import dev.sepler.bytekeeper.rest.PutFileRequest;
+import dev.sepler.bytekeeper.rest.PutFileResponse;
 import javax.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,12 +15,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class ByteKeeperController implements ByteKeeperApi {
 
     @Override
-    public ResponseEntity<GetFileResponse> getFile(@Valid GetFileRequest getFileRequest) {
+    public ResponseEntity<GetFileResponse> getFile(final @Valid GetFileRequest getFileRequest) {
         return ResponseEntity.noContent().build();
     }
 
     @Override
-    public ResponseEntity<GetFilesResponse> getFiles(@Valid GetFilesRequest getFilesRequest) {
+    public ResponseEntity<GetFilesResponse> getFiles(final @Valid GetFilesRequest getFilesRequest) {
+        return ResponseEntity.noContent().build();
+    }
+
+    @Override
+    public ResponseEntity<PutFileResponse> putFile(final @Valid PutFileRequest putFileRequest) {
         return ResponseEntity.noContent().build();
     }
 }
--- tst/dev/sepler/bytekeeper/controller/ByteKeeperControllerTest.java
+++ tst/dev/sepler/bytekeeper/controller/ByteKeeperControllerTest.java
@@ -4,6 +4,8 @@ import dev.sepler.bytekeeper.rest.GetFileRequest;
 import dev.sepler.bytekeeper.rest.GetFileResponse;
 import dev.sepler.bytekeeper.rest.GetFilesRequest;
 import dev.sepler.bytekeeper.rest.GetFilesResponse;
+import dev.sepler.bytekeeper.rest.PutFileRequest;
+import dev.sepler.bytekeeper.rest.PutFileResponse;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.ResponseEntity;
@@ -27,6 +29,13 @@ public class ByteKeeperControllerTest {
     public void getFiles_worksOk() {
         GetFilesRequest getFilesRequest = new GetFilesRequest();
         ResponseEntity<GetFilesResponse> responseEntity = byteKeeperController.getFiles(getFilesRequest);
+        Assertions.assertEquals(204, responseEntity.getStatusCode().value());
+    }
+
+    @Test
+    public void putFile_worksOk() {
+        PutFileRequest putFileRequest = new PutFileRequest();
+        ResponseEntity<PutFileResponse> responseEntity = byteKeeperController.putFile(putFileRequest);
         Assertions.assertEquals(204, responseEntity.getStatusCode().value());
     }
 
