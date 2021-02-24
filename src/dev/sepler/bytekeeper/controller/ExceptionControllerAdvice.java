05ad647328237f6acc62e36afc45dc5f72e56325
sepler <sepler@users.noreply.github.com>
2021-02-24 02:04:12 -0500

    rem @Slf4j

--- src/dev/sepler/bytekeeper/controller/ExceptionControllerAdvice.java
+++ src/dev/sepler/bytekeeper/controller/ExceptionControllerAdvice.java
@@ -2,12 +2,10 @@ package dev.sepler.bytekeeper.controller;
 
 import dev.sepler.bytekeeper.exception.ErrorRequestException;
 import dev.sepler.bytekeeper.rest.ErrorResponse;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-@Slf4j
 @ControllerAdvice
 public class ExceptionControllerAdvice {
 
