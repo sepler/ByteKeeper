143d58c2879fdc40805d535d8bd8080225044b54
sepler <sepler@users.noreply.github.com>
2021-03-24 00:48:54 -0400

    Add cmd args to specify input directory

--- scripts/uploader.py
+++ scripts/uploader.py
@@ -1,22 +1,42 @@
 from clint.textui.progress import Bar
 import errno
+import getopt
 import json
 from pprint import pprint
 import requests
 from requests_toolbelt import MultipartEncoder, MultipartEncoderMonitor
 import shutil
+import sys
 import tempfile
 import urllib3
 import os
 
+# Config
 output_file_name = 'uploader.json'
 excluded_items = ('uploader.json')
 excluded_ext = ('.zip', '.py')
-verify_certs = False
 api_endpoint = 'http://localhost:8080'
+verify_certs = False
+
 
 putfile_method = api_endpoint + '/putFile'
 download_method = api_endpoint + '/download'
+input_directory = '.'
+
+try:
+  opts, args = getopt.getopt(sys.argv[1:], 'hi:', ['help', 'input='])
+except getopt.GetoptError:
+  print('usage: uploader.py [-i input_directory]')
+  sys.exit(2)
+else:
+  for opt, arg in opts:
+    if opt in ('-h', '--help'):
+      print('usage: uploader.py [-i input_directory]')
+      sys.exit(0)
+    elif opt in ('-i', '--input'):
+      input_directory = arg
+      print('Using directory:', input_directory)
+
 if not verify_certs:
   urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
@@ -26,19 +46,20 @@ def create_monitor_callback(encoder):
     progress_bar.show(monitor.bytes_read)
   return callback
 
-all_items = os.listdir('.')
+all_items = os.listdir(input_directory)
 items = list(filter(lambda x: not x.endswith(excluded_ext) and x not in excluded_items, all_items))
 print('Found', len(items), 'items:', items)
 
 uploaded_zips = []
 for index, item in enumerate(items, start=1):
   print('Compressing', item, '...')
-  if os.path.isfile(item):
+  item_with_path = os.path.join(input_directory, item)
+  if os.path.isfile(item_with_path):
     with tempfile.TemporaryDirectory() as tmp_dir:
-      shutil.copy(item, tmp_dir)
+      shutil.copy(item_with_path, tmp_dir)
       shutil.make_archive(item, 'zip', tmp_dir)
   else:
-    shutil.make_archive(item, 'zip', item)
+    shutil.make_archive(item, 'zip', item_with_path)
   zip_name = item + '.zip'
   zip_size_mb = os.path.getsize(zip_name) / (1024 * 1024)
   print('Uploading archive: {}. Size: {:,.5f} MB'.format(zip_name, zip_size_mb))
